cfd77a52f10ab27f49095fdd3c4d14bfac0fe393
intenvy <intenvy.dev@gmail.com>
2022-04-28 17:46:53 +0430

    add: spring initial structure

---
+++ src/main/java/ir/sharif/hideandseek/HideAndSeekApplication.java
@@ -0,0 +1,13 @@
+package ir.sharif.hideandseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HideAndSeekApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HideAndSeekApplication.class, args);
+  }
+
+}
