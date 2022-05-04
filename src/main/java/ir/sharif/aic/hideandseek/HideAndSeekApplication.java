5c25052331488225626cd5161c3bb0df4d1aba2f
intenvy <intenvy.dev@gmail.com>
2022-05-04 20:10:36 +0430

    add: application entry point

---
+++ src/main/java/ir/sharif/aic/hideandseek/HideAndSeekApplication.java
@@ -0,0 +1,11 @@
+package ir.sharif.aic.hideandseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HideAndSeekApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(HideAndSeekApplication.class, args);
+  }
+}
