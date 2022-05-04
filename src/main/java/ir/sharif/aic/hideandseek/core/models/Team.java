83b2d3f88caaadff50ccd5acc0a1b441a24fdbd1
intenvy <intenvy.dev@gmail.com>
2022-05-04 20:47:54 +0430

    add: team model

---
+++ src/main/java/ir/sharif/aic/hideandseek/core/models/Team.java
@@ -0,0 +1,30 @@
+package ir.sharif.aic.hideandseek.core.models;
+
+import ir.sharif.aic.hideandseek.api.grpc.HideAndSeek;
+
+public enum Team {
+  FIRST,
+  SECOND;
+
+  public HideAndSeek.Team toProto() {
+    switch (this) {
+      case FIRST:
+        return HideAndSeek.Team.FIRST;
+      case SECOND:
+        return HideAndSeek.Team.SECOND;
+      default:
+        throw new IllegalArgumentException("invalid team type");
+    }
+  }
+
+  public Team other() {
+    switch (this) {
+      case FIRST:
+        return SECOND;
+      case SECOND:
+        return FIRST;
+      default:
+        throw new IllegalArgumentException("invalid team type");
+    }
+  }
+}
