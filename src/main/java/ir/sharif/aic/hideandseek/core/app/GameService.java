941fa9fd79ea026be004b58474b9b87994b32b8c
intenvy <intenvy.dev@gmail.com>
2022-05-05 18:01:20 +0430

    fix: merge game and game service

--- src/main/java/ir/sharif/aic/hideandseek/core/app/GameService.java
+++ src/main/java/ir/sharif/aic/hideandseek/core/app/GameService.java
@@ -1,8 +1,15 @@
 package ir.sharif.aic.hideandseek.core.app;
 
+import ir.sharif.aic.hideandseek.channel.AsyncChannel;
+import ir.sharif.aic.hideandseek.channel.Channel;
+import ir.sharif.aic.hideandseek.core.event.GameEvent;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
+  private final Channel<GameEvent> eventChannel;
 
+  public GameService() {
+    this.eventChannel = new AsyncChannel<>();
+  }
 }
