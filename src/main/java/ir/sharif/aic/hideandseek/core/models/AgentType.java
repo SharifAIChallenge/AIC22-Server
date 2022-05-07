93be05a36cae04762f79f6cc52b2adb29f093476
intenvy <intenvy.dev@gmail.com>
2022-05-08 00:36:12 +0430

    refactor: add proto to AgentType

--- src/main/java/ir/sharif/aic/hideandseek/core/models/AgentType.java
+++ src/main/java/ir/sharif/aic/hideandseek/core/models/AgentType.java
@@ -3,28 +3,16 @@ package ir.sharif.aic.hideandseek.core.models;
 import ir.sharif.aic.hideandseek.api.grpc.HideAndSeek;
 
 public enum AgentType {
-  POLICE,
-  THIEF;
+  POLICE(HideAndSeek.AgentType.POLICE),
+  THIEF(HideAndSeek.AgentType.THIEF);
 
-  public AgentType otherType() {
-    switch (this) {
-      case POLICE:
-        return THIEF;
-      case THIEF:
-        return POLICE;
-      default:
-        throw new IllegalStateException("invalid agent type");
-    }
+  private final HideAndSeek.AgentType proto;
+
+  AgentType(HideAndSeek.AgentType proto) {
+    this.proto = proto;
   }
 
   public HideAndSeek.AgentType toProto() {
-    switch (this) {
-      case POLICE:
-        return HideAndSeek.AgentType.POLICE;
-      case THIEF:
-        return HideAndSeek.AgentType.THIEF;
-      default:
-        throw new IllegalStateException("invalid agent type");
-    }
+    return this.proto;
   }
 }
