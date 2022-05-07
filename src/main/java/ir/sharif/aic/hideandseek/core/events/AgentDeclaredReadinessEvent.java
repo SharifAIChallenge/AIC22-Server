45bdb074a21edf78557698346400d123d989df93
intenvy <intenvy.dev@gmail.com>
2022-05-08 00:31:01 +0430

    edit: AgentDeclaredReadinessEvent message

--- src/main/java/ir/sharif/aic/hideandseek/core/events/AgentDeclaredReadinessEvent.java
+++ src/main/java/ir/sharif/aic/hideandseek/core/events/AgentDeclaredReadinessEvent.java
@@ -3,7 +3,7 @@ package ir.sharif.aic.hideandseek.core.events;
 public class AgentDeclaredReadinessEvent extends GameEvent {
   public AgentDeclaredReadinessEvent(int agentId, String token) {
     super(GameEventType.READINESS_DECLARATION);
-    this.message = String.format("an agent with id:%d has declared its readiness", agentId);
+    this.message = String.format("agent with id:%d has declared its readiness", agentId);
     this.addContext("agentId", agentId);
     this.addContext("token", token);
   }
