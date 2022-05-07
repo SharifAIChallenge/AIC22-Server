fbe3c181cfaa6fcba6270ea5a4e7fb64f5efc4a7
intenvy <intenvy.dev@gmail.com>
2022-05-08 00:28:11 +0430

    add: turn model

---
+++ src/main/java/ir/sharif/aic/hideandseek/core/models/Turn.java
@@ -0,0 +1,25 @@
+package ir.sharif.aic.hideandseek.core.models;
+
+import ir.sharif.aic.hideandseek.api.grpc.HideAndSeek;
+
+public enum Turn {
+  THIEF_TURN(HideAndSeek.Turn.THIEF_TURN),
+  POLICE_TURN(HideAndSeek.Turn.POLICE_TURN);
+
+  private final HideAndSeek.Turn proto;
+
+  Turn(HideAndSeek.Turn proto) {
+    this.proto = proto;
+  }
+
+  public Turn next() {
+    return switch (this) {
+      case THIEF_TURN -> POLICE_TURN;
+      case POLICE_TURN -> THIEF_TURN;
+    };
+  }
+
+  public HideAndSeek.Turn toProto() {
+    return this.proto;
+  }
+}
