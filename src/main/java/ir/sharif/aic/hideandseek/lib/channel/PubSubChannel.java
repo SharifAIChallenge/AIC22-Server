c1f6da9836feec90378810eb5c44588fbb5daa23
shayan <shayanshabanzadeh0@gmail.com>
2022-07-26 21:46:04 +0430

    fix : add watcher in PubSubChannel.java version : change to version 0.0.4 remove : hideandseek-0.0.1.jar from git

--- src/main/java/ir/sharif/aic/hideandseek/lib/channel/PubSubChannel.java
+++ src/main/java/ir/sharif/aic/hideandseek/lib/channel/PubSubChannel.java
@@ -8,109 +8,120 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PubSubChannel<T> implements Channel<T> {
-  private final List<Watcher<T>> watchers;
-  private final Queue<T> eventQueue;
-  private final Object queueLock;
-  private final AtomicBoolean isClosed;
-  private final Thread backgroundBroadcaster;
+    private final List<Watcher<T>> watchers;
+    private final Queue<T> eventQueue;
+    private final Object queueLock;
+    private final AtomicBoolean isClosed;
+    private final Thread backgroundBroadcaster;
 
-  public PubSubChannel() {
-    this.watchers = Collections.synchronizedList(new ArrayList<>());
-    this.eventQueue = new LinkedBlockingQueue<>();
-    this.queueLock = new Object();
-    this.isClosed = new AtomicBoolean();
+    public PubSubChannel() {
+        this.watchers = Collections.synchronizedList(new ArrayList<>());
+        this.eventQueue = new LinkedBlockingQueue<>();
+        this.queueLock = new Object();
+        this.isClosed = new AtomicBoolean();
 
-    // start the background broadcaster thread
-    this.backgroundBroadcaster = new Thread(this::broadcast);
-    this.backgroundBroadcaster.start();
-  }
-
-  @Override
-  public void addWatcher(Watcher<T> watcher) {
-    this.watchers.add(watcher);
-  }
-
-  @Override
-  public void push(T msg) {
-    // if the channel is closed
-    if (this.isClosed.get()) {
-      // ignore the message
-      return;
+        // start the background broadcaster thread
+        this.backgroundBroadcaster = new Thread(this::broadcast);
+        this.backgroundBroadcaster.start();
     }
 
-    this.eventQueue.add(msg);
-    synchronized (this.queueLock) {
-      this.queueLock.notifyAll();
-    }
-  }
-
-  @Override
-  public void close() {
-    this.isClosed.set(true);
-    try {
-      this.backgroundBroadcaster.join();
-    } catch (InterruptedException ignored) {
-      // the background broadcaster will never be interrupted
-    }
-  }
-
-  private void broadcast() {
-    while (!this.isClosed.get()) {
-      if (this.eventQueue.isEmpty()) {
-        // wait for a new message
-        synchronized (this.queueLock) {
-          try {
-            this.queueLock.wait(200);
-          } catch (InterruptedException ignored) {
-            // this thread will never be interrupted
-          }
-        }
-      }
-      this.processLatestEvent();
-    }
-
-    while (!this.eventQueue.isEmpty()) {
-      this.processLatestEvent();
-    }
-  }
-
-  private void processLatestEvent() {
-    if (this.eventQueue.isEmpty()) {
-      return;
-    }
-
-    var msg = this.eventQueue.poll();
-    var tasks = this.startWatchTasks(msg);
-    this.waitFor(tasks);
-  }
-
-  private List<Thread> startWatchTasks(T msg) {
-    var tasks = new ArrayList<Thread>();
-    for (Watcher<T> watcher : this.watchers) {
-      var task =
-          new Thread(
-              () -> {
+    @Override
+    public void addWatcher(Watcher<T> watcher) {
+        new Runnable() {
+            @Override
+            public void run() {
                 try {
-                  watcher.watch(msg);
-                } catch (Exception ignored) {
-                  // if a watcher throws an error, it means it kinda denied the message
-                  // therefore we ignore it anyways.
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
-              });
-      tasks.add(task);
-      task.start();
-    }
-    return tasks;
-  }
+            }
+        }.run();
+        this.watchers.add(watcher);
 
-  private void waitFor(List<Thread> tasks) {
-    for (Thread task : tasks) {
-      try {
-        task.join();
-      } catch (InterruptedException ignored) {
-        // if a watch task is interrupted, it means that the watcher denied the message
-        // therefore we ignore it anyways.
-      }
     }
-  }
+
+    @Override
+    public void push(T msg) {
+        // if the channel is closed
+        if (this.isClosed.get()) {
+            // ignore the message
+            return;
+        }
+
+        this.eventQueue.add(msg);
+        synchronized (this.queueLock) {
+            this.queueLock.notifyAll();
+        }
+    }
+
+    @Override
+    public void close() {
+        this.isClosed.set(true);
+        try {
+            this.backgroundBroadcaster.join();
+        } catch (InterruptedException ignored) {
+            // the background broadcaster will never be interrupted
+        }
+    }
+
+    private void broadcast() {
+        while (!this.isClosed.get()) {
+            if (this.eventQueue.isEmpty()) {
+                // wait for a new message
+                synchronized (this.queueLock) {
+                    try {
+                        this.queueLock.wait(200);
+                    } catch (InterruptedException ignored) {
+                        // this thread will never be interrupted
+                    }
+                }
+            }
+            this.processLatestEvent();
+        }
+
+        while (!this.eventQueue.isEmpty()) {
+            this.processLatestEvent();
+        }
+    }
+
+    private void processLatestEvent() {
+        if (this.eventQueue.isEmpty()) {
+            return;
+        }
+
+        var msg = this.eventQueue.poll();
+        var tasks = this.startWatchTasks(msg);
+        this.waitFor(tasks);
+    }
+
+    private List<Thread> startWatchTasks(T msg) {
+        var tasks = new ArrayList<Thread>();
+        for (Watcher<T> watcher : this.watchers) {
+            var task =
+                    new Thread(
+                            () -> {
+                                try {
+                                    watcher.watch(msg);
+                                } catch (Exception ignored) {
+                                    // if a watcher throws an error, it means it kinda denied the message
+                                    // therefore we ignore it anyways.
+                                }
+                            });
+            tasks.add(task);
+            task.start();
+        }
+        return tasks;
+    }
+
+    private void waitFor(List<Thread> tasks) {
+        for (Thread task : tasks) {
+            try {
+                task.join();
+            } catch (InterruptedException ignored) {
+                // if a watch task is interrupted, it means that the watcher denied the message
+                // therefore we ignore it anyways.
+            }
+        }
+    }
 }
