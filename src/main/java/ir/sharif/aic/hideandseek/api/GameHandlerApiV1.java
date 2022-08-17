f774fab2e54ad6b7bf09794881d6b48f46c5a2b3
Alireza Ziaee <alrz1999@gmail.com>
2022-08-17 22:22:28 +0430

    add more logs

--- src/main/java/ir/sharif/aic/hideandseek/api/GameHandlerApiV1.java
+++ src/main/java/ir/sharif/aic/hideandseek/api/GameHandlerApiV1.java
@@ -10,16 +10,19 @@ import ir.sharif.aic.hideandseek.core.commands.DeclareReadinessCommand;
 import ir.sharif.aic.hideandseek.core.commands.MoveCommand;
 import ir.sharif.aic.hideandseek.core.commands.WatchCommand;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
 @RequiredArgsConstructor
+@Slf4j
 public class GameHandlerApiV1 extends GameHandlerGrpc.GameHandlerImplBase {
   private final GameService gameService;
 
   @Override
   public void declareReadiness(
       HideAndSeek.DeclareReadinessCommand cmd, StreamObserver<Empty> responseObserver) {
+    log.info("Received readiness command from client {}", cmd.getToken());
     this.gameService.handle(new DeclareReadinessCommand(cmd));
     responseObserver.onNext(Empty.getDefaultInstance());
     responseObserver.onCompleted();
@@ -41,6 +44,8 @@ public class GameHandlerApiV1 extends GameHandlerGrpc.GameHandlerImplBase {
 
   @Override
   public void move(HideAndSeek.MoveCommand cmd, StreamObserver<Empty> responseObserver) {
+    log.info("Received move command from client {} to move to node {}", cmd.getToken(), cmd.getToNodeId());
+
     this.gameService.handle(new MoveCommand(cmd));
     responseObserver.onNext(Empty.getDefaultInstance());
     responseObserver.onCompleted();
@@ -48,6 +53,8 @@ public class GameHandlerApiV1 extends GameHandlerGrpc.GameHandlerImplBase {
 
   @Override
   public void sendMessage(HideAndSeek.ChatCommand cmd, StreamObserver<Empty> responseObserver) {
+    log.info("Received sendMessage command from client {} with text = '{}'", cmd.getToken(), cmd.getText());
+
     this.gameService.handle(new ChatCommand(cmd));
     responseObserver.onNext(Empty.getDefaultInstance());
     responseObserver.onCompleted();
