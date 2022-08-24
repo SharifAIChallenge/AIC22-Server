8b8f5e2b7a2d7acbcccb4637107e48e2bf383e0b
shayan <shayanshabanzadeh0@gmail.com>
2022-08-24 12:24:33 +0430

    refactor: change GrpcEventBroadCaster watch method to ignore all events except Turn_change , Result_change , Status_change and DeclaredReadiness events version: update to version v0.1.7

--- src/main/java/ir/sharif/aic/hideandseek/api/GrpcEventBroadCaster.java
+++ src/main/java/ir/sharif/aic/hideandseek/api/GrpcEventBroadCaster.java
@@ -3,14 +3,14 @@ package ir.sharif.aic.hideandseek.api;
 import io.grpc.stub.StreamObserver;
 import ir.sharif.aic.hideandseek.api.grpc.HideAndSeek;
 import ir.sharif.aic.hideandseek.core.app.GameService;
-import ir.sharif.aic.hideandseek.core.events.GameEvent;
-import ir.sharif.aic.hideandseek.core.events.GameStatusChangedEvent;
-import ir.sharif.aic.hideandseek.core.events.GameTurnChangedEvent;
-import ir.sharif.aic.hideandseek.core.events.PoliceCaughtThiefEvent;
+import ir.sharif.aic.hideandseek.core.events.*;
 import ir.sharif.aic.hideandseek.lib.channel.Watcher;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.java.Log;
+
+import java.util.logging.Logger;
 
 @AllArgsConstructor
 @Builder
@@ -21,13 +21,19 @@ public class GrpcEventBroadCaster implements Watcher<GameEvent> {
     @Getter
     private final StreamObserver<HideAndSeek.GameView> observer;
     private boolean isClosed;
+    private final Logger logger = Logger.getLogger(GrpcEventBroadCaster.class.getName());
 
     @Override
     public void watch(GameEvent event) {
         if (this.isClosed) {
             return;
         }
-
+        if (!(event instanceof GameTurnChangedEvent ||
+                event instanceof GameStatusChangedEvent ||
+                event instanceof GameResultChangedEvent ||
+                event instanceof AgentDeclaredReadinessEvent))
+            return;
+        logger.info("this log is from event : " + event.getType());
         var view = this.gameService.getView(this.agentToken);
         this.observer.onNext(view);
         if (isAgentArrested(event)) {
